7a62698a603edf1e92d7f55c552f577e07a9d0dd
FikrinBende <fikrinbende@gmail.com>
2023-08-14 08:02:23 +0300

    initial commit

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:todo_ddd/domain/auth/value_objects.dart';
+
+import 'auth_failure.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword(
+      {required EmailAddress emailAddress, required Password password});
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
